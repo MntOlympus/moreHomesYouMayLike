3ecfde3224837faf07f4d5f666472b57067c58ce
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-03-17 12:17:51 -0700

    Update conflicts

---
+++ client/components/FavoritePop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const FavoritePop = () => (
+
+<div>FavoritePop</div>
+
+
+);
+
+export default FavoritePop;
---
+++ client/components/RecHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const RecHome = (props) => (
+
+<div>RecHome</div>
+
+
+);
+
+export default RecHome;
