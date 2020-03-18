ad91e82272effcb602cb9fcea06fcf131ce9c59e
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-03-18 11:40:19 -0700

    Simplified testing scope

--- client/index.jsx
+++ client/index.jsx
@@ -2,10 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-const root = document.getElementById('root');
-
-  if (root) {
-    ReactDOM.render(<App />, root);
-  }
-
-export {root};
+ReactDOM.render(<App />, document.getElementById('root'));
