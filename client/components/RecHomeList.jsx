9558243ba34593b7a5147505068f06c3d3ef3b01
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-03-21 00:32:10 -0700

    Code cleanup
    add button css classes for future use.

--- client/components/RecHomeList.jsx
+++ client/components/RecHomeList.jsx
@@ -6,26 +6,27 @@ const RecHomeList = ({ homesSet, shift, nexthome, prevhome }) => {
    const homes = homesSet.map(home => <RecHome key={home._id} home={home} />)
 
     //implement restriction of three home views on screen
-        //implement scroll left/right via buttons
+    //implement scroll left/right via buttons
 
     return (
-            <div>
-                <button
-                    onClick={nexthome}
-                    disabled={shift === homesSet.length-3}
-                    >Forward</button>
-                <button
+        <div>
+                <button className="left-button"
                     onClick={prevhome}
                     disabled={shift === 0}
                 >Back</button>
 
-                <h4>More Homes You May Like</h4>
-                    <div className='list-wrapper' style={{
-                        'transform': `translateX(-${shift*(100/homesSet.length)}%)`
-                        }}>
-                            {homes}
-                    </div>
-            </div>
+                <button className="right-button"
+                    onClick={nexthome}
+                    disabled={shift === homesSet.length-3}
+                    >Forward</button>
+
+            <h4>More Homes You May Like</h4>
+
+                <div className='list-wrapper' style={{
+                    'transform': `translateX(-${shift*(100/homesSet.length)}%)`
+                }}>{homes}</div>
+
+        </div>
     )
 
 };
